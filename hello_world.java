9d98d6fb6fdbf30d3e4233fe6269bfa81a3584d2
Alfelo15 <alfelo15@hotmail.com>
2015-10-05 19:49:41 +0200

    Primer commit

---
+++ hello_world.java
@@ -0,0 +1,21 @@
+//Definició del pakage
+package cat.iesjoaquimmir.myapp.views;
+
+//Declaració d'importacions de classes
+import java.util.Random;
+import java.util.Date;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.WordUtils;
+import static java.lang.System.out;
+import static java.lang.System.gc;
+import static java.lang.System.exit;
+
+//Declaració de la classe
+public class Application {
+
+	//Definició del mètode main
+	public static void main(String[] args) {
+		out.println("Hello World from Java!");
+	
+	}
+}
